cc51f9b8cf8322333b687a1ddddcf1310f79be61
Ján Boldocký <58009306+xboldocky@users.noreply.github.com>
2021-05-05 12:57:39 +0200

    Solving compilation errors

--- src/TurboShield.h
+++ src/TurboShield.h
@@ -1,9 +1,18 @@
+/*
+	API for TurboShield hardware.
+	Initial version uncomplete API in development phase.
+	
+	 This code is part of the AutomationShield hardware and software
+  	ecosystem. Visit http://www.automationshield.com for more
+  	details. This code is licensed under a Creative Commons
+  	Attribution-NonCommercial 4.0 International License.
+*/
 #ifndef TURBOSHIELD_H_
 #define TURBOSHIELD_H_
-#include<Arduino.h>
-#include<Wire.h>
-
-class TurboShieldClass(){
+#include"Arduino.h"
+#include"Wire.h"
+#include"AutomationShield.h"
+class TurboShieldClass{
 public:
 	void begin();
 	float sensorRead();
